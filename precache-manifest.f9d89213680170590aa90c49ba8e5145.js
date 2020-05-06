8304f58de1bdbf8cf6bf5084db315e05f1c2b8c2
Damiano Chintala <damianochintala@gmail.com>
2020-05-06 19:42:05 +0200

    Updates

---
+++ precache-manifest.f9d89213680170590aa90c49ba8e5145.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd579c9831a5a6326e4d7645176dc1dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26d487f83a31ad8635b9",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "da4fa4aef204a8a2f26b",
+    "url": "/static/js/2.ba31a84a.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.ba31a84a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26d487f83a31ad8635b9",
+    "url": "/static/js/main.1e8531ec.chunk.js"
+  },
+  {
+    "revision": "38e770178e6548ddb964",
+    "url": "/static/js/runtime-main.6460c8b8.js"
+  },
+  {
+    "revision": "9af88f2a7c234e63619cd06dafb23b1b",
+    "url": "/static/media/civic.9af88f2a.jpg"
+  },
+  {
+    "revision": "e61ce49164281bec094e32118c551483",
+    "url": "/static/media/education.e61ce491.jpg"
+  },
+  {
+    "revision": "a57ee3e27419d548e9e416808a47cd24",
+    "url": "/static/media/enterprise.a57ee3e2.jpg"
+  },
+  {
+    "revision": "fff7402aac4c231c4b94cbeac6a94d6f",
+    "url": "/static/media/logo.fff7402a.png"
+  },
+  {
+    "revision": "bd1cb014151287c73b61f898f4c1f154",
+    "url": "/static/media/logo2.bd1cb014.png"
+  },
+  {
+    "revision": "3f6770a9f073463d5f109001d7f09534",
+    "url": "/static/media/noah.3f6770a9.jpg"
+  },
+  {
+    "revision": "f7a8d32e7cc8cd6978f28b419b6fe431",
+    "url": "/static/media/skills.f7a8d32e.jpg"
+  },
+  {
+    "revision": "9b2553886462e3d0dc56c355234181e1",
+    "url": "/static/media/team1.9b255388.jpg"
+  },
+  {
+    "revision": "a96adc2709d93876b8283a4a1546fcaf",
+    "url": "/static/media/team2.a96adc27.jpg"
+  }
+]);
