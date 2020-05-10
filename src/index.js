4c63d481e4a1e7256dd71af4ddeb56eb16d96ce9
Damiano Chintala <damianochintala@gmail.com>
2020-05-10 11:41:26 +0200

    resolving eslint warnings

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// eslint-disable-next-line
 import $ from 'jquery';
+// eslint-disable-next-line
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './index.css';
+// eslint-disable-next-line
 import { HashRouter } from "react-router-dom";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
